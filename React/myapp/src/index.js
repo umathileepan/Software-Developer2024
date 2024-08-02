a06a43a447702e62849fd2d8e1dab0937c207503
umathileepan <thileepan_uma@yahoo.co.uk>
2024-08-02 21:55:53 +0100

    React committed

---
+++ React/myapp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App(){
+  return(
+    <h1>Welcome to our website</h1>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
